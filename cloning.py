644e5e7135d13fb950004baa65553e9f8666e079
Ochieng' Meshack Otieno <146129953+Meshack12756@users.noreply.github.com>
2024-02-18 14:33:31 +0300

    Create cloning.py

---
+++ cloning.py
@@ -0,0 +1,4 @@
+#it is also possible to clone strings using the concept of character indexing
+name = "My name is Ochieng' Meshack"
+second_name = name[:]#clones the string of the variable"name"
+print(second_name)
