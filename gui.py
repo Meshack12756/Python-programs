c2a2b568dae4d4c3a9933c844fa94c11c273f9f0
Ochieng' Meshack Otieno <146129953+Meshack12756@users.noreply.github.com>
2025-03-07 01:47:26 -0800

    Add files via upload
    A Graphical User Interface in Python using Tkinter. Implements a Mortgage loan calculation

---
+++ gui.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+
+#Mortgage function to run upon submit
+def submit():
+    loan = int(entry_a.get())
+    term = int(entry_b.get())
+    rate = float(entry_c.get())
+    down = int(entry_d.get())
+
+    months = term * 12
+    rate_monthly = rate / 100 / 12
+    payment = (rate_monthly / (1- (1+ rate_monthly) ** (-months))) * (loan - down)
+    result = tk.Label(text = "$" + str(round(payment,2)))
+    result.pack()
+
+# Create Tkinter window GUI
+window = tk.Tk()
+
+#Creating Tkinter and entry boxes
+label_a = tk.Label(text = "Please Enter loan amount: ")
+label_a.pack()
+entry_a = tk.Entry()
+entry_a.pack()
+label_b = tk.Label(text = "Please Enter loan term: ")
+label_b.pack()
+entry_b = tk.Entry()
+entry_b.pack()
+label_c = tk.Label(text = "Please Enter the loan rate: ")
+label_c.pack()
+entry_c = tk.Entry()
+entry_c.pack()
+label_d = tk.Label(text = "Please Enter the down payment : ")
+label_d.pack()
+entry_d = tk.Entry()
+entry_d.pack()
+
+#Submit Button
+MyButton = tk.Button(window, text = "Submit", width=10, command=submit)
+MyButton.pack()
+
+# Run the loop
+window.mainloop()
