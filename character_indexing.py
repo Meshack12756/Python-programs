9a255a836fb35bc72b87b7f3fa94933af2427a0b
Ochieng' Meshack Otieno <146129953+Meshack12756@users.noreply.github.com>
2025-03-11 12:46:04 -0700

    Update character_indexing.py

--- character_indexing.py
+++ character_indexing.py
@@ -1,7 +1,7 @@
 name = "My name is Ochieng' Meshack"
 print(name[0])#only prints out the first character of the string in the variable "name"
 print(name[-2])#only prints out the second-last character of the string in the variable "name"
-print(name[0:3])#prints out the first,second and third character of the string in the variable"name"
-print(name[0:])#prints the entire string
-print(name[1:])#prints the entire string apart from the first character
-print(name[:3])#prints out the first,second and third character of the string in the variable"name"
+print(name[0:3])#prints out the first,second and third character of the string in the variable "name"
+print(name[0:])#prints the whole string
+print(name[1:])#prints the whole string apart from the first character
+print(name[:3])#prints out the first,second and third character of the string in the variable "name"
